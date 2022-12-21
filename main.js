ec772389b8af7baa8ade825ab92726d76d00a912
liksonbrother082527@gmail.com <liksonbrother082527@gmail.com>
2022-12-21 16:29:22 +0600

    ok

---
+++ main.js
@@ -0,0 +1,17 @@
+var bn1, bn2, en1, en2, mth, bgs, sci, isl, ict, agri, numShower;
+bn1 = document.querySelector('.row:nth-child(2) .col-2 input');
+bn2 = document.querySelector('.row:nth-child(3) .col-2 input');
+en1 = document.querySelector('.row:nth-child(4) .col-2 input');
+en2 = document.querySelector('.row:nth-child(5) .col-2 input');
+mth = document.querySelector('.row:nth-child(6) .col-2 input');
+bgs = document.querySelector('.row:nth-child(7) .col-2 input');
+sci = document.querySelector('.row:nth-child(8) .col-2 input');
+isl = document.querySelector('.row:nth-child(9) .col-2 input');
+ict = document.querySelector('.row:nth-child(10) .col-2 input');
+agri = document.querySelector('.row:nth-child(11) .col-2 input');
+numShower = document.querySelector('.total-mark');
+function count(){
+var totalMark =Number(bn1.value) + Number(bn2.value) + Number(en1.value) + Number(en2.value) + Number(mth.value) + Number(bgs.value) + Number(sci.value) + Number(ict.value) + Number(agri.value);
+
+numShower.value = totalMark
+}
