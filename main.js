6ea8e7ea195e1cd27b97f167b506a39940cc2fd2
liksonbrother082527@gmail.com <liksonbrother082527@gmail.com>
2022-12-21 17:19:04 +0600

    ok

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var bn1, bn2, en1, en2, mth, bgs, sci, isl, ict, agri, numShower;
+var bn1, bn2, en1, en2, mth, bgs, sci, isl, ict, agri, numShower, allSubj;
 bn1 = document.querySelector('.row:nth-child(2) .col-2 input');
 bn2 = document.querySelector('.row:nth-child(3) .col-2 input');
 en1 = document.querySelector('.row:nth-child(4) .col-2 input');
@@ -10,8 +10,14 @@ isl = document.querySelector('.row:nth-child(9) .col-2 input');
 ict = document.querySelector('.row:nth-child(10) .col-2 input');
 agri = document.querySelector('.row:nth-child(11) .col-2 input');
 numShower = document.querySelector('.total-mark');
-function count(){
-var totalMark =Number(bn1.value) + Number(bn2.value) + Number(en1.value) + Number(en2.value) + Number(mth.value) + Number(bgs.value) + Number(sci.value) + Number(ict.value) + Number(agri.value);
 
+allSubj = document.querySelectorAll('.row .col-2 input');
+allSubj.forEach((subjects)=>{
+  subjects.addEventListener('input', ()=>{
+    count();
+  })
+})
+function count(){
+var totalMark = Number(bn1.value) + Number(bn2.value) + Number(en1.value) + Number(en2.value) + Number(mth.value) + Number(bgs.value) + Number(sci.value) + Number(isl.value) + Number(ict.value) + Number(agri.value);
 numShower.value = totalMark
 }
