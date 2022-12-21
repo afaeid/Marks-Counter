c397fbf1630eda4af79c7777105fd98766d70513
liksonbrother082527@gmail.com <liksonbrother082527@gmail.com>
2022-12-21 21:22:16 +0600

    ok

--- main.js
+++ main.js
@@ -11,13 +11,23 @@ ict = document.querySelector('.row:nth-child(10) .col-2 input');
 agri = document.querySelector('.row:nth-child(11) .col-2 input');
 numShower = document.querySelector('.total-mark');
 
-allSubj = document.querySelectorAll('.row .col-2 input');
-allSubj.forEach((subjects)=>{
-  subjects.addEventListener('input', ()=>{
+allMark = document.querySelectorAll('.row .col-2 input');
+allMark.forEach((marks)=>{
+  marks.addEventListener('input', ()=>{
     count();
+if(Number(marks.value) >= Number(marks.dataset.aplus_mark)){
+  marks.style.background = '#fff';
+  marks.style.color = '#000';
+  marks.style.border = '1px solid #000';
+  }
+if(Number(marks.value) <= Number(marks.dataset.aplus_mark)){
+  marks.style.background = '#FF2A2A';
+  marks.style.color = '#fff';
+  marks.style.border = '1px solid #FF2A2A';
+  }
   })
 })
 function count(){
 var totalMark = Number(bn1.value) + Number(bn2.value) + Number(en1.value) + Number(en2.value) + Number(mth.value) + Number(bgs.value) + Number(sci.value) + Number(isl.value) + Number(ict.value) + Number(agri.value);
-numShower.value = totalMark
+numShower.value = totalMark;
 }
